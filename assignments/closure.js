5808e2f0b9990a2f75fa84241bc803f32a1f7f8f
Christopher Hernandez <cerberean661@gmail.com>
2019-07-23 12:57:01 -0700

    beginning

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const firstName = 'Christopher';
+function introduction(){
+  const surName = 'Hernandez';
+  return("My name is " + {firstName} + " " + {surname} + ".");
+}
+console.log(firstName);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
